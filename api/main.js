d9cc71049919a1c95079e2ad3fce882c7f7090b2
abhay2132 <abhaybro768@gmail.com>
2022-08-31 11:26:08 +0530

    added express

--- api/main.js
+++ api/main.js
@@ -1,7 +1,11 @@
 const {createServer } = require("http");
 const port = process.env.PORT || 3000;
+const exp = require("express");
+const app = exp();
 
-module.exports = createServer((req, res) =>{
-	res.end("Hi Sir, Your server started at port " + port);
+app.get("*", (req,res) =>{
+	res.end("YOUR SERVER IS RUNNIN AT PORT " + port);
 })
+
+module.exports = createServer(app)
 .listen(port, () => console.log("Server started at ", (port)));
